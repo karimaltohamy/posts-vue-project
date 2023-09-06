939536fbbb476caf149b69f0aa99b02e120b3c89
Karim Altohamy <karimaltohamy@Karims-MacBook-Air.local>
2023-09-07 01:57:09 +0200

    commmit

---
+++ src/components/Filters.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="mt-3">
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="search..."
+        @input="handleSearch"
+      />
+      <select class="form-select sort" id="inputGroupSelect01" @change="handleSort">
+        <option selected>Choose...</option>
+        <option value="asc">ascending</option>
+        <option value="desc">Descending</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "filterView",
+  props: {
+    filters: Object
+  },
+  emits: ["set-filters"],
+  setup(props, context) {
+    console.log(props.filters);
+    const handleSearch = (e) => {
+      context.emit("set-filters", {
+        ...props.filters,
+        search: e.target.value,
+      });
+    };
+
+    const handleSort = (e) => {
+        context.emit("set-filters", {
+        ...props.filters,
+        sort: e.target.value,
+      });
+    }
+    return { handleSearch, handleSort };
+  },
+};
+</script>
+
+<style>
+.sort {
+  flex: unset !important;
+  min-width: 200px !important;
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,44 +1,97 @@
 <template>
-  <div class="products">
+  <div class="products pb-3">
     <div class="container">
       <h3>products</h3>
 
-      <div class="boxs" v-if="products">
+      <Filters :filters="filters" @set-filters="handleFilter" />
+      <div class="boxs" v-if="filtersProducts">
         <ProductCard
-          v-for="(product, index) in products"
+          v-for="(product, index) in filtersProducts.length > 0
+            ? filtersProducts
+            : products"
           :key="index"
           :product="product"
         />
+      </div>
+      <div class="text-center">
+        <button class="btn btn-primary mt-5 m-auto" @click="handlePerPage">
+          Lead More
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, reactive, ref, watch } from "vue";
 import { useStore } from "vuex";
 import ProductCard from "../components/ProductCard.vue";
+import Filters from "../components/Filters.vue";
+
 export default {
   name: "productsPage",
   components: {
     ProductCard,
+    Filters,
   },
   setup() {
     const store = useStore();
     const products = computed(() => {
-      return store.state.products.slice(0, 50);
+      return store.state.products.slice(0, perPage.value * 10);
     });
+    const filtersProducts = ref([]);
+    const filters = reactive({
+      search: "",
+      sort: "",
+    });
+    const perPage = ref(1);
 
     const getAllProducts = async () => {
       store.dispatch("getAllProducts");
-      
+      filtersProducts.value = store.state.products.slice(0, perPage.value * 10);
     };
 
     onMounted(() => {
       getAllProducts();
+      console.log(products.value);
     });
 
-    return { products };
+    const handleFilter = (f) => {
+      filters.search = f.search;
+      filters.sort = f.sort;
+
+      let newProducts = products.value.filter((product) =>
+        product.title.toLowerCase().includes(f.search.toLowerCase())
+      );
+
+      if (filters.sort === "asc" || filters.sort === "desc") {
+        newProducts.sort((a, b) => {
+          const sortOrder = filters.sort === "asc" ? 1 : -1;
+          const aValue = a.publishedAt;
+          const bValue = b.publishedAt;
+
+          if (aValue > bValue) {
+            return sortOrder;
+          } else if (aValue < bValue) {
+            return -sortOrder;
+          } else {
+            return 0;
+          }
+        });
+      }
+
+      filtersProducts.value = newProducts;
+    };
+
+    watch([perPage], () => {
+      getAllProducts();
+    });
+
+    const handlePerPage = () => {
+      perPage.value = perPage.value + 1;
+    };
+
+    return { products, filters, handleFilter, filtersProducts, handlePerPage };
   },
 };
 </script>
